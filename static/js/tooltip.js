597db50131d5a48bdaf7bebb598f9eab8237514d
Adam <adam@studentrentit.com>
2014-07-23 09:52:02 -0700

    fix special amenity jumping js

--- static/js/tooltip.js
+++ static/js/tooltip.js
@@ -3,5 +3,5 @@ enable tooltip
 separate js file because bootstrap states it has performance issues when applying to all pages
 */
 $(function () {
-    $("[data-toggle='tooltip']").tooltip();
+    $("[data-toggle='tooltip']").tooltip({container: 'body'});
 });
