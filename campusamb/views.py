d691bb9f0217d120a12603c658705237091b2b6d
Adam <awwester@gmail.com>
2014-09-24 13:49:16 -0700

    base styles for prop/content/support

--- campusamb/views.py
+++ campusamb/views.py
@@ -2,30 +2,65 @@ from django.shortcuts import render
 
 
 def home(request):
-    # home page for the campus ambassadors
+    # informational page for people looking to become campus ambassadors
 
     return render(request, "cacontent/home.html", {})
 
 
 def dashboard(request):
-    # home page for the campus ambassadors
+    # place for campus ambassadors to view vital statistics
 
     return render(request, "cacontent/dashboard.html", {})
 
 
-def property(request):
-    # home page for the campus ambassadors
+def add_property(request):
+    '''
+    campus ambassadors will be able to add new properties into their
+    assigned school list
+    '''
+    action = 'add'
 
-    return render(request, "cacontent/property.html", {})
+    return render(request, "cacontent/property.html",
+        {'action':action})
 
 
-def content(request):
-    # home page for the campus ambassadors
+def edit_property(request):
+    '''
+    campus ambassadors will be able to edit existing properties that
+    fall into their assigned schools
+    '''
+    action = 'edit'
 
-    return render(request, "cacontent/content.html", {})
+    return render(request, "cacontent/property.html",
+        {'action':action})
+
+
+def add_content(request):
+    '''
+    campus ambassadors will be able to add new content such as articles,
+    events and deals
+    '''
+    action = 'add'
+
+    return render(request, "cacontent/content.html",
+        {'action':action})
+
+
+def edit_content(request):
+    '''
+    campus ambassadors will be able to edit existing content items that
+    fall into their assigned schools
+    '''
+    action = 'edit'
+
+    return render(request, "cacontent/content.html",
+        {'action':action})
 
 
 def support(request):
-    # home page for the campus ambassadors
+    '''
+    help for campus ambassadors. Will be FAQ as well as some
+    processes
+    '''
 
     return render(request, "cacontent/support.html", {})
