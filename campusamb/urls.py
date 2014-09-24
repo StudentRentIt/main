eac2c591627e3b0124a702f5a56cff5a0d830674
Adam <awwester@gmail.com>
2014-09-23 21:06:18 -0700

    fix to ca urls

--- campusamb/urls.py
+++ campusamb/urls.py
@@ -6,7 +6,7 @@ app_prefix = 'ca-'
 
 urlpatterns = patterns('',
     url(r'^$', views.home, name=app_prefix + 'home'),
-    url(r'^dashboard$', views.dashboard, name=app_prefix + 'dashboard'),
-    url(r'^property', views.dashboard, name=app_prefix + 'property'),
-    url(r'^content$', views.dashboard, name=app_prefix + 'school-items'),
+    url(r'^dashboard/$', views.dashboard, name=app_prefix + 'dashboard'),
+    url(r'^property/$', views.dashboard, name=app_prefix + 'property'),
+    url(r'^content/$', views.dashboard, name=app_prefix + 'school-items'),
 )
