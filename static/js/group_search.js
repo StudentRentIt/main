7692d4a2876f8bcef3081c1307382f5d91627a9d
Adam <awwester@gmail.com>
2014-09-28 22:06:09 -0700

    add/remove to group search button

--- static/js/group_search.js
+++ static/js/group_search.js
@@ -1,5 +1,7 @@
 $( document ).ready( function (){
-    $( ".btn-add-group-search" ).unbind().click( function() {
+    // add or remove a property from group search
+    $('.btn-add-group-search').off('click');
+    $('.btn-add-group-search').on('click', function() {
         id = $( this ).data( "id" );
 
         $.post('/search/group/property/',
@@ -8,10 +10,14 @@ $( document ).ready( function (){
 
         // change to remove button
         if ( $( this ).hasClass( "btn-add" ) ){
-            $( this ).html( "<span class='glyphicon glyphicon-minus'></span>" );
+            $( this ).html( '<span class="glyphicon glyphicon-plus"></span>' );
+            $( this ).addClass( "btn-remove" ).removeClass( "btn-add" );
+            $( this ).attr( "data-original-title", "Add to Group Search" );
         }
         else if ( $( this ).hasClass( "btn-remove" ) ){
-            $( this ).html( "<span class='glyphicon glyphicon-plus'></span>" );
+            $(this).html( "<span class='glyphicon glyphicon-minus'></span>" );
+            $( this ).addClass( "btn-add" ).removeClass( "btn-remove" );
+            $( this ).attr( "data-original-title", "Remove from Group Search" );
         }
     });
 });
