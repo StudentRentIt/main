36ce064885602f91a50ab807433ccc34ce43a80f
Adam Wester <awwester@gmail.com>
2015-09-29 16:07:18 -0600

    import after settings

--- proj/wsgi.py
+++ proj/wsgi.py
@@ -9,12 +9,12 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 
 import os
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", os.environ["SETTINGS_LOCATION"])
 
 # TODO: this should use whitenoise in deployed instances
 if os.environ['SETTINGS_LOCATION'] != 'settings.local_adam':
+    from whitenoise.django import DjangoWhiteNoise
     application = DjangoWhiteNoise(application)
 else:
     application = get_wsgi_application()
